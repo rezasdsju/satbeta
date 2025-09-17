ff3e5d5ac0d66814dbc924e60a363ef95f151508
rezasdsju <rezaul2002104@gmail.com>
2025-09-17 23:44:37 +0600

    satbeta urls data

--- satbeta/urls.py
+++ satbeta/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),  # <-- home.urls include আছে কি check করো
+    path('data/', include('data.urls')),
 ]
